3a0a42d498d8e1a2a6ea4ac06a2dc95aee80d628
Quant Oracle <51112808+quantOracle@users.noreply.github.com>
2019-06-12 22:41:35 +0300

    Create CreditRisk.h

---
+++ CreditRisk.h
@@ -0,0 +1,20 @@
+#ifndef __FinancialSamples__CreditRisk__
+#define __FinancialSamples__CreditRisk__
+// A simple class representing a credit risk assessment
+class CreditRisk {
+public:
+ // these are risk grades, as determined by rating agencies
+ enum RiskType {
+ AAA,
+ AAPlus,
+ AA,
+ APlus,
+ A,
+ BPlus,
+ B,
+ CPlus,
+ C
+ };
+ };
+
+#endif /* defined(__FinancialSamples__CreditRisk__) */
